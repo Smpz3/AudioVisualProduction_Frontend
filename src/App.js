44ca13307141304b69b4eb30ea06f01d24d20670
Sofia <paezsofiam@gmail.com>
2023-07-15 20:36:29 +0200

    Added Bootstrap. Basic creation of NavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <NavBar/> 
   );
 }
+
 
 export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+export default function NavBar() {
+    return <nav classname="nav">
+        <a href="/" classname="site-title">TBSP Site</a>
+        <ul> 
+            <li>
+                <a href="/shows">Shows</a>
+                <a href="/audio">Audio</a>
+                <a href="/store">Store</a>
+                <a href="/haracters">Characters</a>
+            </li>
+        </ul>
+    </nav>
+}
