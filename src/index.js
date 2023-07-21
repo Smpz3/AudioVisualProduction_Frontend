5635b3c61e857fe73763edf07e4f47908cdfab1d
Joan Marti <joanmartigamez@gmail.com [core]>
2023-07-21 23:44:39 +0200

    interceptor for localstorage token. Its commented

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 
 import App from './App';
+
+// Axios interceptors
+
+// axios.interceptors.request.use((request) => {
+
+//     if (localStorage.getItem('user_token')) {
+//         request.headers.Authorization = localStorage.getItem('user_token');
+//     }
+//     return request;
+// });
+
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- 
-    <App/>
-  
-); 
+    <App />
+);
 
 
 
