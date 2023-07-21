8370ff4aef6578f26cf988f7cb3260ff1a8d90f7
Joan Marti <joanmartigamez@gmail.com [core]>
2023-07-21 10:49:57 +0200

    testt

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
-import { BrowserRouter,  Route,  Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar /> 
-      <Routes> 
-        <Route path='/' element={<Shows />}/>
-        <Route path='/' element={<Characters />}/>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Shows />} />
+        <Route path='/' element={<Characters />} />
 
-     
-      </Routes > 
+
+      </Routes >
       <div className="page-container">
         <div className="content-wrap">
-          
+
         </div>
         <Footer />
       </div>
     </BrowserRouter>
-    
+
   );
 }
 
