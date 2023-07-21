25c9fc5f6cfada868afab62034b47040ae4a5e6c
Joan Marti <joanmartigamez@gmail.com [core]>
2023-07-21 21:40:51 +0200

    modified baseUrl

--- src/services/admin.services.js
+++ src/services/admin.services.js
@@ -1,10 +1,10 @@
 import axios from "axios"
 
-const baseUrl = 'http://localhost:3000/api/users/register'
+const baseUrl = 'http://localhost:3000/api/users/'
 
 const insertUser = async (values) => {
     try {
-        const response = await axios.post(baseUrl, values);
+        const response = await axios.post(`${baseUrl}/register`, values);
         return response.data;
     } catch (error) {
         return error.message;
