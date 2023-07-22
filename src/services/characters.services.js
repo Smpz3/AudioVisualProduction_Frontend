e706cb57fe8e2244c590eebca9c8c4611b62227b
Joan Marti <joanmartigamez@gmail.com [core]>
2023-07-22 13:17:49 +0200

    get all characters querys & services

---
+++ src/services/characters.services.js
@@ -0,0 +1,14 @@
+import axios from "axios"
+
+const baseUrl = 'http://localhost:3000/api/characters/'
+
+const getCharacters = async () => {
+    try {
+        const response = await axios.get(baseUrl);
+        return response.data;
+    } catch (error) {
+        return error.message;
+    };
+};
+
+export { getCharacters }
