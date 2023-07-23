675fb7ab362003185f6f94baff9f798ce27f8a16
Sofia <paezsofiam@gmail.com>
2023-07-23 17:11:08 +0200

    changed alignment of the home images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,9 @@ const ImageContainer = styled.div`
   justify-content: center;
   align-items: center;
   width: 100%; 
-  border-top: 2px solid pink; 
+  border-top: 2px solid pink;
+ margin-top: 30px; 
+  
 `;
 
 
@@ -31,12 +33,7 @@ const Text = styled.p`
   text-align: center;
 
 `;
-const Line = styled.div`
-  width: 40%;
-  height: 2px;
-  background-color: pink;
-  margin: 20px 0;
-`;
+
 
 const Title = styled.h3`
   font-size: 24px;
@@ -44,6 +41,13 @@ const Title = styled.h3`
   color: var(--secondaryColor); 
   display: flex; 
 `;
+const ImageRcontainer = styled.div` 
+display: flex; 
+justify-content:flex-end; 
+width: 100%; 
+
+`; 
+
 
 const image1URL = "https://media.licdn.com/dms/image/D4E03AQFQ_O2xZmohLw/profile-displayphoto-shrink_200_200/0/1673061580437?e=1695254400&v=beta&t=vIxS8L68XYG0ueY7HA-h68EOiR25JMVEnU5wpSYmhcM";
 const image2URL = "https://drive.google.com/uc?id=1oX6Xa46YMzWfwRWYUToFZJc5SH_7OO3n";
@@ -63,9 +67,11 @@ const Home = () => {
                     <Image src={image1URL} alt="Image 1" />
             <Text>The Brightside Productions LLC is a multidisciplinary production company and research institute created in order to incorporate the application of evidence-based research and social work practices within the creative field through music, animation, television, film, and other mediums.  </Text>
                 </div>
-                <Line/>
-                <div>
-                    <Image src={image2URL} alt="Image 2" />
+                
+          <div>
+            <ImageRcontainer>
+              <Image src={image2URL} alt="Image 2" />
+            </ImageRcontainer>
             <Text>The primary objective of The Brightside Productions LLC is to create a global social impact so that all individuals can live healthier, happier, and more knowledgeable lives. Children ages 8-12 years-old in the United States spend an average of 4 to 6 hours per day staring at a screen while teenagers strikingly spend up to 9 hours a day on screen time.  </Text>
                 </div>
             </ImageContainer>
@@ -73,8 +79,10 @@ const Home = () => {
                 <Image src={image3URL} alt="Image 3" />
           <Text>While the negative effects of such extensive screen time warrant a separate and lengthy discussion - we have an opportunity to articulate and demonstrate critical narratives for childhood and adulthood development through this identified branch of communication. Thus, we aspire to create an objective way to accomplish these goals through the creation of an animated television series centered around evidence-based research and social work practices.</Text>
             </div>
-            <div>
-                <Image src={image4URL} alt="Image 4" />
+        <div>
+          <ImageRcontainer>
+            <Image src={image4URL} alt="Image 4" />
+          </ImageRcontainer>
           <Text>Areas of interest for the television series will include interpersonal communication skills, trauma and resilience informed approaches, mastery of environment through systems theory, mental health disorders and wellness, and general psychology principles. Influencing social change requires a deep understanding of the problems facing society and steadfastness to overcome them which led to the creation of our advisory panel/editorial board </Text>
             </div>
             
