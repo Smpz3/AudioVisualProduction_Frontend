09739f508ecc7ec5b28a1a93e1b44bbc00c9424b
Joan Marti <joanmartigamez@gmail.com [core]>
2023-07-25 11:43:40 +0200

    some fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -50,7 +50,7 @@ display: flex;
 justify-content:flex-end; 
 width: 100%; 
 
-`; 
+`;
 
 
 const image1URL = "https://media.licdn.com/dms/image/D4E03AQFQ_O2xZmohLw/profile-displayphoto-shrink_200_200/0/1673061580437?e=1695254400&v=beta&t=vIxS8L68XYG0ueY7HA-h68EOiR25JMVEnU5wpSYmhcM";
@@ -59,39 +59,39 @@ const image3URL = "https://media.licdn.com/dms/image/C5616AQGY9JRJ2miYZA/profile
 const image4URL = "https://media.licdn.com/dms/image/D563DAQEPMw8GW-P70Q/image-scale_191_1128/0/1673395795668?e=1690837200&v=beta&t=fJazaxdz2LwVVyeA2dHyUfwTP6By-lwxSyXq0ojM8SY ";
 
 const Home = () => {
-    
 
-    return (
-        <Container>
-            <Title> <h3> Welcome! Learn more about us here:</h3>
-            </Title>
-            
-            <ImageContainer>
-                <div>
-                    <Image src={image1URL} alt="Image 1" />
-            <Text>The Brightside Productions LLC is a multidisciplinary production company and research institute created in order to incorporate the application of evidence-based research and social work practices within the creative field through music, animation, television, film, and other mediums.  </Text>
-                </div>
-                
-          <div>
-            <ImageRcontainer>
-              <Image src={image2URL} alt="Image 2" />
-            </ImageRcontainer>
-            <Text>The primary objective of The Brightside Productions LLC is to create a global social impact so that all individuals can live healthier, happier, and more knowledgeable lives. Children ages 8-12 years-old in the United States spend an average of 4 to 6 hours per day staring at a screen while teenagers strikingly spend up to 9 hours a day on screen time.  </Text>
-                </div>
-            </ImageContainer>
-            <div>
-                <Image src={image3URL} alt="Image 3" />
-          <Text>While the negative effects of such extensive screen time warrant a separate and lengthy discussion - we have an opportunity to articulate and demonstrate critical narratives for childhood and adulthood development through this identified branch of communication. Thus, we aspire to create an objective way to accomplish these goals through the creation of an animated television series centered around evidence-based research and social work practices.</Text>
-            </div>
+
+  return (
+    <Container>
+      <Title> <h3> Welcome! Learn more about us here:</h3>
+      </Title>
+
+      <ImageContainer>
+        <div>
+          <Image src={image1URL} alt="Image 1" />
+          <Text>The Brightside Productions LLC is a multidisciplinary production company and research institute created in order to incorporate the application of evidence-based research and social work practices within the creative field through music, animation, television, film, and other mediums.  </Text>
+        </div>
+
         <div>
           <ImageRcontainer>
-            <Image src={image4URL} alt="Image 4" />
+            <Image src={image2URL} alt="Image 2" />
           </ImageRcontainer>
-          <Text>Areas of interest for the television series will include interpersonal communication skills, trauma and resilience informed approaches, mastery of environment through systems theory, mental health disorders and wellness, and general psychology principles. Influencing social change requires a deep understanding of the problems facing society and steadfastness to overcome them which led to the creation of our advisory panel/editorial board </Text>
-            </div>
-            
-        </Container>
-    );
+          <Text>The primary objective of The Brightside Productions LLC is to create a global social impact so that all individuals can live healthier, happier, and more knowledgeable lives. Children ages 8-12 years-old in the United States spend an average of 4 to 6 hours per day staring at a screen while teenagers strikingly spend up to 9 hours a day on screen time.  </Text>
+        </div>
+      </ImageContainer>
+      <div>
+        <Image src={image3URL} alt="Image 3" />
+        <Text>While the negative effects of such extensive screen time warrant a separate and lengthy discussion - we have an opportunity to articulate and demonstrate critical narratives for childhood and adulthood development through this identified branch of communication. Thus, we aspire to create an objective way to accomplish these goals through the creation of an animated television series centered around evidence-based research and social work practices.</Text>
+      </div>
+      <div>
+        <ImageRcontainer>
+          <Image src={image4URL} alt="Image 4" />
+        </ImageRcontainer>
+        <Text>Areas of interest for the television series will include interpersonal communication skills, trauma and resilience informed approaches, mastery of environment through systems theory, mental health disorders and wellness, and general psychology principles. Influencing social change requires a deep understanding of the problems facing society and steadfastness to overcome them which led to the creation of our advisory panel/editorial board. </Text>
+      </div>
+
+    </Container>
+  );
 };
 
 export default Home;
